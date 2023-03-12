f900b1135260b5ab3b06d6f13f8ff48436dec8bd
lacnoskillz <kaikane3457@hotmail.com>
2023-03-12 01:28:47 -0600

    resized icons and centered them at the bottom of the page. added simple form to take name email and message in contact.js made a header.css that arnt being used yet

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,9 +4,22 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        Contact me here
-      </p>
+      <form>
+      <div class="mb-3">
+    <label for="" class="form-label">Name</label>
+    <input type="name" class="form-control" id="inputname"></input>
+  </div>
+  <div class="mb-3">
+    <label for="" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="inputemail" aria-describedby="emailHelp"></input>
+  </div>
+  <div class="mb-3">
+  <label for="" class="form-label">Message</label>
+  <textarea class="form-control" id="inputmessage" rows="3"></textarea>
+</div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     </div>
   );
 }
--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -3,14 +3,17 @@ import React from 'react';
 import { FaLinkedin, FaGithub} from "react-icons/fa";
 export default function About() {
   return (
-    <section class="footer">
-    <div class="links">
-      <a href="https://www.linkedin.com/in/kaikane-lacno-91601b268/"><FaLinkedin /></a>
-      <a href="https://github.com/lacnoskillz"><FaGithub /></a>
-      
-
-    </div>
-  </section>
+    <ul class="nav justify-content-center fixed-bottom ">
+    <li class="nav-item">
+      <a class="nav-link"  href="https://www.linkedin.com/in/kaikane-lacno-91601b268/"><FaLinkedin size='5em'/></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://github.com/lacnoskillz"><FaGithub size='5em'/></a>
+    </li>
+    {/* <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li> */}
+  </ul>
   );
 }
 
