97b439535c2e7e03d66354604ee880ca0540f78f
lacnoskillz <kaikane3457@hotmail.com>
2023-03-12 21:53:35 -0500

    added required attribute to form inputs in Contact.js/ restyles about.css and contact css. also started basic styling for portfolio.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,22 @@
 import React from 'react';
-
+import '../../styles/Contact.css'
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <form>
+    <div className='Contactcontainer'>
+      <div><h1>Contact Me</h1>
+      </div>
+      <form required>
       <div className="mb-3">
-    <label for="" className="form-label">Name</label>
-    <input type="name" className="form-control" id="inputname"></input>
+    <label  className="form-label">Name</label>
+    <input type="name" className="form-control" id="inputname" required></input>
   </div>
   <div className="mb-3">
-    <label for="" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="inputemail" aria-describedby="emailHelp"></input>
+    <label  className="form-label">Email address</label>
+    <input type="email" className="form-control" id="inputemail" aria-describedby="emailHelp" required></input>
   </div>
   <div className="mb-3">
-  <label for="" className="form-label">Message</label>
-  <textarea className="form-control" id="inputmessage" rows="3"></textarea>
+  <label  className="form-label">Message</label>
+  <textarea className="form-control" id="inputmessage" rows="10" required></textarea>
 </div>
 
   <button type="submit" className="btn btn-primary">Submit</button>
