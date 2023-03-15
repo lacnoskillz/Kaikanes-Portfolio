70e28693888284032f7ad321e0ab397783dbbaff
lacnoskillz <kaikane3457@hotmail.com>
2023-03-14 22:04:59 -0500

    added flex styles for project/Portfolio.css and attempted to fix contact.js to display message when user clicks off dom and input fields are empty

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../../styles/Contact.css'
 export default function Contact() {
+  function Contacthandler(event){
+    const inputId = event.target.getAttribute("id");
+    if(inputId.input === ''){
+      console.log("enter fields for", inputId)
+    }
+
+
+  }
   return (
     <div className='Contactcontainer'>
       <div><h2>Contact Me</h2>
@@ -8,11 +16,11 @@ export default function Contact() {
       <form required>
       <div className="mb-3">
     <label  className="form-label">Name</label>
-    <input type="name" className="form-control" id="inputname" required></input>
+    <input type="name" className="form-control" id="name" onBlur={Contacthandler()}></input>
   </div>
   <div className="mb-3">
     <label  className="form-label">Email address</label>
-    <input type="email" className="form-control" id="inputemail" aria-describedby="emailHelp" required></input>
+    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" required></input>
   </div>
   <div className="mb-3">
   <label  className="form-label">Message</label>
@@ -21,10 +29,12 @@ export default function Contact() {
 
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
+<div className='eroorme'></div>
     </div>
+    
   );
 }
-// use this for click event?
+
 // document.onclick = e => {
 //   if (e.currentTarget !== document.forms[0])
 //     document.forms[0].reset()
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,8 +3,16 @@ import '../../styles/Project.css'
 export default function project() {
   return (
     <div className='projectDiv'>
-      <h2>Projectname</h2>
-      <p>text</p>
+  <div className="card text-center">
+  <div className="card-header">
+    Projectname
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Project title</h5>
+    <p className="card-text">details about project</p>
+    <a href="123" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </div>
   );
 }
